ef24f8fe77b523aa576af755f9b112de10e7106e
zyadhany <zeyad.hany2003@gmail.com>
2024-08-10 00:30:03 +0300

    node #2

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,13 +1,11 @@
 export default function taskBlock(trueOrFalse) {
-    const task = false;
-    const task2 = true;
-  
-    if (trueOrFalse) {
-      // disable prev change
-      const task = true;
-      // disable prev change
-      const task2 = false;
-    }
-  
-    return [task, task2];
+  const task = false;
+  const task2 = true;
+
+  if (trueOrFalse) {
+    const task = true;
+    const task2 = false;
   }
+
+  return [task, task2];
+}
