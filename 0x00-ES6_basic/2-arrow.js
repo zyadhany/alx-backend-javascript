43c89a3413e217d16e472052a8d5acf812d6f1db
zyadhany <zeyad.hany2003@gmail.com>
2024-08-10 00:46:09 +0300

    node #2

--- 0x00-ES6_basic/2-arrow.js
+++ 0x00-ES6_basic/2-arrow.js
@@ -1,9 +1,8 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
 
-  const self = this;
-  this.addNeighborhood = (newNeighborhood) => {
-    self.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return self.sanFranciscoNeighborhoods;
+  this.addNeighborhood = (name) => {
+    this.sanFranciscoNeighborhoods.push(name);
+    return this.sanFranciscoNeighborhoods;
   };
 }
