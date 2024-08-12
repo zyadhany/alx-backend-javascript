af39a08059dcec93d1bd6ec60083989b0f5cbea1
zyadhany <zeyad.hany2003@gmail.com>
2024-08-13 01:39:56 +0300

    added task in promises

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,13 +1,10 @@
-import { rejects } from "assert";
-import { resolve } from "path";
-
 export default function getFullResponseFromAPI(success) {
-    return new Promise((resolve, reject) => {
-        if (success) {
-            resolve({
-                status: 200,
-                body: 'Success',
-            });
-        } else reject(new Error("The fake API is not working currently"));
-    });
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve({
+        status: 200,
+        body: 'Success',
+      });
+    } else reject(new Error('The fake API is not working currently'));
+  });
 }
