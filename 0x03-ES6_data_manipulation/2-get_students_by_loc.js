88faea7deb16ea12918d188ad375c615739e3ccc
zyadhany <zeyad.hany2003@gmail.com>
2024-08-16 14:22:53 +0300

    added task in data manipuation

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,9 +1,3 @@
 export default function getStudentsByLocation(students, location) {
-  function filtration(student) {
-    return (student.location === location);
-  }
-
-  const firtStud = students.filter((student) => (student.location === location));
-
-  return firtStud;
+  return students.filter((student) => (student.location === location));;
 }
