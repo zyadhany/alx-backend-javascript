7491432a1817ccdf0cbde96eea351a48435bb315
zyadhany <zeyad.hany2003@gmail.com>
2024-08-16 14:10:49 +0300

    added task in data manipuation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,11 +1,7 @@
 export default function getListStudentIds(list) {
-  const listids = [];
-
   if (!Array.isArray(list)) return ([]);
 
-  for (const stud of list) {
-    listids.push(stud.id);
-  }
+  const listids = list.map((stud) => stud.id);
 
   return (listids);
 }
