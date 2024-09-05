1cb4b95dd93b81b6fa0880487005f41bdfc06f4e
zyadhany <zeyad.hany2003@gmail.com>
2024-09-06 01:35:30 +0300

    A class

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,17 +1,40 @@
 export default class HolbertonCourse {
   constructor(name, length, students) {
-    if (typeof name !== 'string') {
+    this.name = name;
+    this.length = length;
+    this.students = students;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(newName) {
+    if (typeof newName !== 'string') {
       throw new Error('Name must be a string');
     }
-    if (typeof length !== 'number') {
+    this._name = newName;
+  }
+
+  get length() {
+    return this._length;
+  }
+
+  set length(newLength) {
+    if (typeof newLength !== 'number') {
       throw new Error('Length must be a number');
     }
-    if (!Array.isArray(students)) {
+    this._length = newLength;
+  }
+
+  get students() {
+    return this._students;
+  }
+
+  set students(newStudents) {
+    if (!Array.isArray(newStudents)) {
       throw new Error('Students must be an array');
     }
-
-    this._name = name;
-    this._length = length;
-    this._students = students;
-    };
+    this._students = newStudents;
+  }
 }
