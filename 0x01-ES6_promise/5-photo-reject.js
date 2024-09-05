27543268df979ac983955997bbad46b693c71e91
zyadhany <zeyad.hany2003@gmail.com>
2024-09-05 23:14:34 +0300

    js promises

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,3 +1,3 @@
 export default function uploadPhoto(filename) {
-  return Promise.reject(new Error(`${filename} cannot be processes`));
+  return Promise.reject(new Error(`${filename} cannot be processed`));
 }
--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,3 +1,6 @@
-export default function uploadPhoto(filename) {
+import signUpUser from './4-user-promise'
+import uploadPhoto from './5-photo-reject'
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.reject(new Error(`${filename} cannot be processed`));
 }
