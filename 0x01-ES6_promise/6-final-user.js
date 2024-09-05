4311dbf87d0ecee4042a5819d8da881189e28f83
zyadhany <zeyad.hany2003@gmail.com>
2024-09-05 23:22:43 +0300

    js promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,6 +1,13 @@
-import signUpUser from './4-user-promise'
-import uploadPhoto from './5-photo-reject'
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.reject(new Error(`${filename} cannot be processed`));
+  return Promise
+    .allSettled([signUpUser(fileName, lastName), uploadPhoto(fileName)])
+    .then((res) => {
+      res.map((val) => ({
+        status: val.status,
+        value: val.status === 'fulfilled' ? val.value : String(val.reason),
+      }));
+    });
 }
