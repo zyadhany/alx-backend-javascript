f10a8fcc9462274a5c7b0ec666422b33161684a8
zyadhany <zeyad.hany2003@gmail.com>
2024-09-05 23:24:23 +0300

    js promises

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -3,7 +3,7 @@ import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise
-    .allSettled([signUpUser(fileName, lastName), uploadPhoto(fileName)])
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
     .then((res) => (
       res.map((val) => ({
         status: val.status,
