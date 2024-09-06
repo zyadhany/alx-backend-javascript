7f1d6853818d48bef462c6b79dfcc264fd93af9e
zyadhany <zeyad.hany2003@gmail.com>
2024-09-06 11:24:30 +0300

    clases

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,18 +1,19 @@
 export default class Building {
-  constructor (sqft) {
+  constructor(sqft) {
+    if (this.constructor !== Building && !this.evacuationWarningMessage) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
     this.sqft = sqft;
   }
 
-  get sqft () {
+  get sqft() {
     return this._sqft;
   }
 
-  set sqft (newSqft) {
+  set sqft(newSqft) {
     if (typeof newSqft !== 'number') {
       throw new Error('Sqft must be a number');
     }
     this._sqft = newSqft;
   }
-
-  
 }
