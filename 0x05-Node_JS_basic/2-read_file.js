527b97aeb7980318d181a0c9dcd6f110d8f5fbcc
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-09-30 23:56:59 +0000

    node

--- 0x05-Node_JS_basic/2-read_file.js
+++ 0x05-Node_JS_basic/2-read_file.js
@@ -12,6 +12,33 @@ module.exports = function read(dataPath) {
     .toString('utf-8')
     .trim()
     .split('\n');
-  
-  console.log(fileLines);
+
+  const fields = {};
+  const students = {};
+  for (const line of fileLines) {
+    const items = line.split(',');
+    if (fileLines.indexOf(line) === 0) {
+      for (const item of items) {
+        fields[items.indexOf(item)] = item;
+      }
+    } else {
+      const student = {};
+      for (const item of items) {
+        student[fields[items.indexOf(item)]] = item;
+      }
+      if (student.firstname !== 'firstname') {
+        if (!students[student.field]) {
+          students[student.field] = [];
+        }
+        students[student.field].push(student);
+      }
+    }
+  }
+
+  console.log(`Number of students: ${fileLines.length - 1}`);
+  for (const field in students) {
+    if (field) {
+      console.log(`Number of students in ${field}: ${students[field].length}. List: ${students[field].map((student) => student.firstname).join(', ')}`);
+    }
+  }
 };
