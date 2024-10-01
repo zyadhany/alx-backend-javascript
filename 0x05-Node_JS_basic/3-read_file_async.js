75eb33c9f0dbbb4f47bf3531b8b09577e2614f20
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-10-01 13:35:43 +0000

    node js 3

---
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+
+module.exports = function read(file) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(file, 'utf8', (err, data) => {
+      if (err) {
+        reject(new Error('Cannot load the database'));
+      }
+      if (data) {
+        const lines = data.split('\n').slice(1, -1);
+        console.log(`Number of students: ${lines.length}`);
+        const fields = {};
+        for (const line of lines) {
+          const stud = line.split(',');
+          if (stud.length === 4) {
+            if (!fields[stud[3]]) fields[stud[3]] = [];
+            fields[stud[3]].push(stud[0]);
+          }
+        }
+        for (const field in fields) {
+          if (fields[field]) {
+            console.log(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+          }
+        }
+        resolve(true);
+      }
+    });
+  });
+};
