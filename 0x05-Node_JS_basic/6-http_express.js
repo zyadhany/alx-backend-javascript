d4c10426804b7356572230325786766aab8a99c2
Zyad Hany <102915260+zyadhany@users.noreply.github.com>
2024-10-01 16:01:32 +0000

    express

---
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 1245;
+const HOST = 'localhost';
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(PORT, HOST, () => {
+  console.log(`Server is running on http://${HOST}:${PORT}`);
+});
